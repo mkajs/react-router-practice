d144b1efae620783577c7d8ce75cc348e34d1fbb
Mahdi <kazemi.dist@gmail.com>
2024-07-06 10:35:05 +0330

    feat: :sparkles: Add loading skeleton and its component to app.

--- client/src/AppLayout.jsx
+++ client/src/AppLayout.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Skeleton from "./components/Skeleton";
 
 const AppLayout = () => {
+  const { state } = useNavigation();
   return (
     <div className="max-w-[680px] mx-auto">
       <Navbar />
-      <Outlet />
+      {state === "loading" ? <Skeleton /> : <Outlet />}
     </div>
   );
 };
