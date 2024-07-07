edcdb7aaf293c9035ae7c941bb4b84dfed77aaef
Mahdi <kazemi.dist@gmail.com>
2024-07-07 17:06:43 +0330

    refactor: :art: refactor the fetch function in loaders.js.

--- client/src/loaders.js
+++ client/src/loaders.js
@@ -1,31 +1,27 @@
-export async function postsLoader() {
-  const posts = await fetch(`${import.meta.env.VITE_API_URL}/posts`).then(
+async function getData(urlPath) {
+  const result = await fetch(`${import.meta.env.VITE_API_URL}${urlPath}`).then(
     (res) => res.json()
   );
+  return result;
+}
+export async function postsLoader() {
+  const posts = await getData(`/posts`);
   return { posts };
 }
 export async function usersLoader() {
-  const users = await fetch(`${import.meta.env.VITE_API_URL}/users`).then(
-    (res) => res.json()
-  );
+  const users = await getData(`/users`);
   return { users };
 }
 export async function userLoader({ params }) {
-  const user = await fetch(
-    `${import.meta.env.VITE_API_URL}/users/${params.id}`
-  ).then((res) => res.json());
+  const user = await getData(`/users/${params.id}`);
   return { user };
 }
 export async function todosLoader() {
-  const todos = await fetch(`${import.meta.env.VITE_API_URL}/todos`).then(
-    (res) => res.json()
-  );
+  const todos = await getData(`/todos`);
   return { todos };
 }
 
 export async function postLoader({ params }) {
-  const post = await fetch(
-    `${import.meta.env.VITE_API_URL}/posts/${params.id}`
-  ).then((res) => res.json());
+  const post = await getData(`/posts/${params.id}`);
   return { post };
 }
